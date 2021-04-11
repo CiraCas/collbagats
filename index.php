accea3638ac464ddc2d1e9c46c24fc84c54d9843
CiraCas <zyra_cas@hotmail.com>
2021-04-11 21:35:26 +0200

    icons && font-awesome library

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="es">
 
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>CollbaGats</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -17,14 +17,23 @@
   <!-- Place favicon.ico in the root directory -->
 
   <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
   <link rel="stylesheet" href="css/main.css">
 
   <meta name="theme-color" content="#fafafa">
 </head>
 
 <body>
-
+  <head>
+    <div class="iconos">
+      <i class="fab fa-facebook-f"></i>
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-instagram"></i>
+      <i class="fas fa-lock"></i>
+    </div>
   <p>Gatete bonito!</p>
+  <h1>página para los gatetes de Collbató</h1>
+  </head>
   <script src="js/vendor/modernizr-3.11.2.min.js"></script>
   <script src="js/vendor/jquery-3.6.0.min.js"></script>
   <script src="js/plugins.js"></script>
